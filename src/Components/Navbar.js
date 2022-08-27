04e43927bf3081bc0270d3405e8a9fe6295222d8
Joacoalnabulsi <96397160+Joacoalnabulsi@users.noreply.github.com>
2022-08-27 17:17:30 -0300

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+
+export default function Navbar() {
+    return (
+        <nav>
+             <img src={'/imgs/airbnb-logo.png'} />
+        </nav>
+        
+    )
+}
